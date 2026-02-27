e5b44429f10f6a32e06889ea1e5fef43682052f7
Shovas <shovas@shovas.ca>
2026-02-26 22:30:29 -0500

    Add project files.

---
+++ gtr2-memory-operations-tool-wpf/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace gtr2_memory_operations_tool_wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ gtr2-memory-operations-tool-wpf/ViewModels/TabControlViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using gtr2_memory_operations_tool_wpf.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace gtr2_memory_operations_tool_wpf.ViewModels
+{
+    internal class TabControlViewModel : ObservableObject
+    {
+        public ObservableCollection<TabItem> TabItems { get; set; }
+        public TabControlViewModel()
+        {
+            TabItems = new ObservableCollection<TabItem>();
+            var home = new TabItem { Header = "Home", Content = new HomeView() };
+            var data = new TabItem { Header = "Data", Content = new DataView() };
+            var log = new TabItem { Header = "Log", Content = new LogView() };
+            TabItems.Add(home);
+            TabItems.Add(data);
+            TabItems.Add(log);
+        }
+    }
+}
